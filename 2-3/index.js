ee0fe8cd74628e468332a41bec4600449b25fb07
nimabht <nimabeheshtaein99@gmail.com>
2023-02-05 22:30:22 +0330

    2-3 modified.

--- 2-3/index.js
+++ 2-3/index.js
@@ -1,48 +1,27 @@
-const fs = require("fs/promises");
+const fs = require("fs");
+const { generateText } = require("./tools/generateText");
 const namesPath = "./names.txt";
 const numbersPath = "./numbers.txt";
 
-const generateText = (users) => {
-  let result = [];
-  users.map((user) => {
-    if (user.numbers.length === 0) {
-      result.push(`${user.name} hasn't any phone number.`);
-    }
-    if (user.numbers.length === 1) {
-      result.push(`${user.name}'s phone number is ${user.numbers[0]}`);
-    }
-    if (user.numbers.length > 1) {
-      result.push(`${user.name}'s phone numbers are ${user.numbers.join()}`);
-    }
-  });
-  return result.join("\n");
-};
-
-function main() {
-  let users = [];
-  fs.readFile(namesPath, "utf8")
-    .then((data) => {
-      data.split("\r\n").forEach((user) => {
-        let temp = user.split("-");
-        users.push({ id: temp[0], name: temp[1], numbers: [] });
-      });
-      fs.readFile(numbersPath, "utf8")
-        .then((data) => {
-          data.split("\r\n").forEach((number) => {
-            let temp = number.split("-");
-            let indexOfUser = users.findIndex((user) => user.id === temp[0]);
-            if (indexOfUser > -1) users[indexOfUser].numbers.push(temp[1]);
-          });
-          console.log(users);
-          fs.writeFile("./result.txt", generateText(users));
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    })
-    .catch((err) => {
-      console.log(err);
+async function main() {
+  try {
+    const users = [];
+    const usersText = await fs.promises.readFile(namesPath, "utf8");
+    const numbersText = await fs.promises.readFile(numbersPath, "utf8");
+    usersText.split("\r\n").forEach((user) => {
+      let temp = user.split("-");
+      users.push({ id: +temp[0], name: temp[1], numbers: [] });
     });
-}
+    numbersText.split("\r\n").forEach((number) => {
+      let temp = number.split("-");
+      let indexOfUser = users.findIndex((user) => user.id == temp[0]);
+      if (indexOfUser > -1) users[indexOfUser].numbers.push(temp[1]);
+    });
 
+    await fs.promises.writeFile("./result.txt", generateText(users));
+    console.log("Done!");
+  } catch (error) {
+    console.log(error);
+  }
+}
 main();
